131807f7cad131592ab532d36ad5f38b4473bcb6
이창준 <2018110021@dongguk.edu>
2024-07-29 03:07:03 +0900

    design: 달력 상단부 배치 및 디자인 변경

--- Dev/Web/src/components/Calendar/style.tsx
+++ Dev/Web/src/components/Calendar/style.tsx
@@ -17,13 +17,35 @@ export const StyleCalendar = styled(Calendar)`
 
   .react-calendar__navigation {
     display: flex;
-    height: 24px;
+    justify-content: center; /* 가운데 정렬 */
+    align-items: center; /* 수직 정렬 */
+    height: 34px; /* 높이를 늘려서 가운데 정렬이 더 명확하게 보이도록 함 */
     margin-bottom: 1em;
+    gap: 0.5em; /* 버튼과 라벨 사이의 간격을 줄임 */
+  }
+
+  .react-calendar__navigation__label {
+    flex-grow: 0 !important;
+
+    gap: 0.5em; /* 월/연도와 버튼 사이의 간격을 줄임 */
+    font-size: 1.5em; /* 크기를 키움 */
+    color: #42850d; /* 텍스트 색상 설정 */
+    padding: 0; /* 불필요한 패딩 제거 */
+  }
+
+  .react-calendar__navigation__label > span {
+    margin: 0;
+    padding: 0;
   }
 
   .react-calendar__navigation button {
     min-width: 24px;
     background: none;
+    border: none; /* 버튼 테두리 제거 */
+    padding: 0; /* 불필요한 패딩 제거 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   .react-calendar__navigation button:disabled {
@@ -42,7 +64,6 @@ export const StyleCalendar = styled(Calendar)`
     font-size: 1em; /* 크기를 키움 */
     color: #42850d; /* 텍스트 색상 설정 */
     border-top: 1px solid #42850d; /* 요일 테두리 색상 */
-
     border-bottom: 1px solid #42850d; /* 요일 테두리 색상 */
   }
 
@@ -50,7 +71,6 @@ export const StyleCalendar = styled(Calendar)`
     color: #42850d; /* 주말 텍스트 색상 설정 */
   }
 
-  // 달력 내부 타일 (1일 ~ 31일) 크기 조정
   .react-calendar__tile {
     padding: 2em; /* 패딩 조정 */
     font-size: 1.2em; /* 폰트 크기 조정 */
@@ -89,12 +109,10 @@ export const StyleCalendar = styled(Calendar)`
     font-size: 1em;
     width: auto;
     height: auto;
-
     background: #ffffff; /* 오늘 텍스트 배경 색상 */
   }
 
   .react-calendar__tile--active {
-    // background: #42850d; /* 선택된 날짜 배경 색상 */
     color: #ffffff; /* 선택된 날짜 텍스트 색상 */
     border-radius: 5px;
   }
