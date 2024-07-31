54a85144011fc349b96accf66f92e2de1da8c6a1
이창준 <2018110021@dongguk.edu>
2024-07-31 16:45:57 +0900

    remove: Unused code

--- Dev/Web/src/App.tsx
+++ Dev/Web/src/App.tsx
@@ -1,23 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
-import { useState } from "react";
-import Modal from "./components/Modal";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
   return (
     <BrowserRouter>
       <Router />
-      <button onClick={openModal}>모달 열기</button>
-      {isModalOpen && <Modal onClose={closeModal} />}
     </BrowserRouter>
   );
 }
