89b8d6897589375a5c57d86cd9feb83c9a85e0d9
이창준 <2018110021@dongguk.edu>
2024-08-01 16:43:57 +0900

    remove: Unused code

--- Dev/Web/src/App.tsx
+++ Dev/Web/src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
-import NavBar from "@/components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
-        <NavBar/>
     </BrowserRouter>
   );
 }
