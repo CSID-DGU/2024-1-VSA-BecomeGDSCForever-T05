9727145d7a5d4a464064db981407a6ce52a6405d
Changjoon <127469340+CWDll@users.noreply.github.com>
2024-08-01 19:04:31 +0900

    ✨ Feature - Top Menu Bar Component (#17)
    * feat: navBar 기본 레이아웃 구성
    
    * feat: 참여중인 모들락이 있을 때 상단 메뉴 바에 표시한다
    
    * design: 기본 프로필 이미지 생성
    
    * remove: Unused code
    
    * feat: home에 NavBar 적용

--- Dev/Web/src/pages/Home/index.tsx
+++ Dev/Web/src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import SizedBox from "@/components/Common/SizedBox";
 import {useSelector} from "react-redux";
 import {RootState} from "@/stores/store.ts";
 import Answer from "@/components/Answer";
+import NavBar from "@/components/NavBar";
+import Row from "@/components/Common/Row";
 
 export default function Home() {
 
@@ -12,9 +14,12 @@ export default function Home() {
 
     return (
         <Styled.Container>
-            {isOpen ? <Answer/> : <MyCalendar/>}
-            <SizedBox width={"64px"}/>
-            <Question/>
+            <NavBar/>
+            <Row>
+                {isOpen ? <Answer/> : <MyCalendar/>}
+                <SizedBox width={"64px"}/>
+                <Question/>
+            </Row>
         </Styled.Container>
     )
 }
--- Dev/Web/src/pages/Home/style.ts
+++ Dev/Web/src/pages/Home/style.ts
@@ -4,6 +4,7 @@ export const Container = styled.div`
     display: flex;
     justify-content: start;
     align-items: start;
+    flex-direction: column;
     //max-height: 1080px;
     //overflow: hidden;
     max-width: 1920px;
