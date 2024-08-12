d4ed91e2f718129ca61739fea77da8560651e26c
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-08-13 00:26:23 +0900

    chore: Spring Project Start

---
+++ Dev/API-Server/src/test/java/org/dongguk/vsa/modeul/ApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongguk.vsa.modeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
