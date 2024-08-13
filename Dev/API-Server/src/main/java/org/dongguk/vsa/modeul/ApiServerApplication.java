d29d5e494c30769318fd9b6fd7661a5dd9bc9173
jjuuuunnii <jake991110@naver.com>
2024-08-13 20:17:02 +0900

    feat: mongo, mysql 범위 지정

--- Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/ApiServerApplication.java
+++ Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/ApiServerApplication.java
@@ -2,7 +2,17 @@ package org.dongguk.vsa.modeul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableJpaRepositories(basePackages = {
+        "org.dongguk.vsa.modeul.dialogue",
+        "org.dongguk.vsa.modeul.keyword",
+        "org.dongguk.vsa.modeul.modeullak",
+        "org.dongguk.vsa.modeul.tag",
+        "org.dongguk.vsa.modeul.user"
+})
+@EnableMongoRepositories(basePackages = "org.dongguk.vsa.modeul.file")
 @SpringBootApplication
 public class ApiServerApplication {
 
