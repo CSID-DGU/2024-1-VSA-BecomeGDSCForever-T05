5507f3805c66c496086daf45a561147839f6822a
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-08-15 17:43:32 +0900

    feat: App Config

---
+++ Dev/App/lib/app/config/app_dependency.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+/// This class is used to manage the dependencies of the application.
+///
+/// You can add your dependencies here.
+class AppDependency extends Bindings {
+  @override
+  void dependencies() {
+    // Add your mediator dependencies here
+
+    // Add your provider dependencies here
+
+    // Add your repository dependencies here
+  }
+}
---
+++ Dev/App/lib/app/config/app_pages.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+
+/// GetPage is a configuration class that is used to define a route in GetMaterialApp.
+abstract class AppPages {
+  static List<GetPage> data = [];
+}
