5624df0ac054d84bb98616afc7b32f06858e7dd8
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-08-17 17:51:59 +0900

    feat: Mail System Connection

--- Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/core/contants/Constants.java
+++ Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/core/contants/Constants.java
@@ -17,6 +17,19 @@ public class Constants {
     public static String ACCESS_TOKEN = "access_token";
     public static String REFRESH_TOKEN = "refresh_token";
 
+    // RABBIT MQ
+    public static final String MODEUL_EXCHANGE_NAME = "modeul.exchange";
+
+    public static final String MODEUL_USER_QUEUE_NAME = "user.queue";
+    public static final String MODEUL_DIALOGUE_QUEUE_NAME = "dialogue.queue";
+    public static final String MODEUL_STORAGE_QUEUE_NAME = "storage.queue";
+
+    public static final String MODEULLAK_DIALOGUE_ROUTING_KEY = "modeullaks.*.dialogues";
+    public static final String MODEULLAK_STORAGE_CONTENT_ROUTING_KEY = "modeullaks.*.storages.*.content";
+    public static final String MODEULLAK_USER_ROUTING_KEY = "modeullaks.*.users";
+    public static final String MODEULLAK_USER_STORAGE_ROUTING_KEY = "modeullaks.*.users.*.storages";
+    public static final String MODEULLAK_USER_DIALOGUE_ROUTING_KEY = "modeullaks.*.users.*.dialogues";
+
     /**
      * 인증이 필요 없는 URL
      */
--- Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/core/exception/handler/StompGlobalExceptionHandler.java
+++ Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/core/exception/handler/StompGlobalExceptionHandler.java
@@ -1,9 +1,54 @@
 package org.dongguk.vsa.modeul.core.exception.handler;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.dongguk.vsa.modeul.core.exception.type.CommonException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class StompGlobalExceptionHandler {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    /**
+     * Stomp 전용 인증/인가 이후 Controller, Service, Repository 에서 발생하는 CommonException 예외 처리
+     *
+     * @param clientMessage 클라이언트 메시지
+     * @param e 사용자 실수 예외
+     */
+    @MessageExceptionHandler(CommonException.class)
+    public void handleCommonException(
+            Message<byte[]> clientMessage,
+            CommonException e
+    ) {
+        log.error("GlobalExceptionHandler catch CommonException In Stomp Processing : {}", e.getMessage());
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(clientMessage);
+
+        // TODO: 오류 발생 시 생길 Logic 구현해야 함
+    }
+
+    /**
+     * Stomp 전용 인증/인가 이후 Controller, Service, Repository 에서 발생하는 Exception 예외 처리
+     *
+     * @param clientMessage 클라이언트 메시지
+     * @param e 사용자 실수 예외
+     */
+    @MessageExceptionHandler(Exception.class)
+    public void handleException(
+            Message<byte[]> clientMessage,
+            Exception e
+    ) {
+        log.error("GlobalExceptionHandler catch Exception In Stomp Processing : {}", e.getMessage());
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(clientMessage);
+
+        // TODO: 오류 발생 시 생길 Logic 구현해야 함
+    }
 }
