92fe91386ac8c5fbf0e2005531607b419ec85e89
jjuuuunnii <jake991110@naver.com>
2024-08-22 01:40:13 +0900

    feat: 5-8. 모들락 대화 질문 생성/수정하기 API 구현

---
+++ Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/dialogue/usecase/UpdateDialogueQuestionUseCase.java
@@ -0,0 +1,19 @@
+package org.dongguk.vsa.modeul.dialogue.usecase;
+
+import org.dongguk.vsa.modeul.dialogue.dto.request.UpdateDialogueQuestionRequestDto;
+
+import java.util.UUID;
+
+public interface UpdateDialogueQuestionUseCase {
+
+    /**
+     * dialogue의 질문을 생성합니다.
+     * @param updateDialogueQuestionRequestDto dialogue 질문 생성 요청 DTO
+     * @Param dialogueId dialogue ID
+     */
+    void execute(
+            UpdateDialogueQuestionRequestDto updateDialogueQuestionRequestDto,
+            UUID accountId,
+            Long dialogueId
+    );
+}
