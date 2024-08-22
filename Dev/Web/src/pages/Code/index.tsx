f36a6ce74ab212f0a2d574720a075fea35d633ba
이창준 <2018110021@dongguk.edu>
2024-08-22 18:32:51 +0900

    resolve conflict

--- Dev/Web/src/pages/Code/index.tsx
+++ Dev/Web/src/pages/Code/index.tsx
@@ -1,11 +1,11 @@
 import * as Styled from "./style.ts"
 import IDEMulti from "@/components/Sidebar/IDEMulti";
 import IDEPersonal from "@/components/Sidebar/IDEPersonal";
-import DefaultRoom from "@/components/Room/DefaultRoom";
-import UserRoom from "@/components/Room/UserRoom";
 import {useSelector} from "react-redux";
 import {RootState} from "@/stores/store.ts";
 import {useParams} from "react-router-dom";
+import CodeIde from "@/components/Ide/CodeIde";
+
 
 export default function Code() {
 
@@ -20,9 +20,13 @@ export default function Code() {
             <IDEMulti/>
             <IDEPersonal/>
             {
-                RoomFrameState === "default" ? <DefaultRoom modeullakId={parseInt(modeullakId)}/> :
-                    <UserRoom modeullakId={parseInt(modeullakId)}/>
+                // RoomFrameState === "default" ? <DefaultRoom modeullakId={parseInt(modeullakId)}/> :
+                //     <UserRoom modeullakId={parseInt(modeullakId)}/>
+
+                // RoomFrameState === "default" ? <DefaultRoom/> : <UserRoom/>
+
             }
+            <CodeIde/>
         </Styled.Container>
     );
 }
