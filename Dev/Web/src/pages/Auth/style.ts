76ffad642bf843b4618edbeea4f721a26f6ca0a3
이창준 <2018110021@dongguk.edu>
2024-08-22 21:00:37 +0900

    Design: Login View

--- Dev/Web/src/pages/Auth/style.ts
+++ Dev/Web/src/pages/Auth/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
+    height: 100%;
     margin: 0 auto;
     display: flex;
     justify-content: center;
