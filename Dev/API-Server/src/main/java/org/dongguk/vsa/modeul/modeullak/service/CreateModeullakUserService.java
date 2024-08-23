39caf5bd89417e0a125fbfaa75b3250e19637438
HyungJoon <kr.dev.hyungjoon@gmail.com>
2024-08-23 17:37:43 +0900

    feat: Stomp Setting

--- Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/modeullak/service/CreateModeullakUserService.java
+++ Dev/API-Server/src/main/java/org/dongguk/vsa/modeul/modeullak/service/CreateModeullakUserService.java
@@ -1,6 +1,7 @@
 package org.dongguk.vsa.modeul.modeullak.service;
 
 import lombok.RequiredArgsConstructor;
+import org.dongguk.vsa.modeul.core.contants.Constants;
 import org.dongguk.vsa.modeul.core.exception.error.ErrorCode;
 import org.dongguk.vsa.modeul.core.exception.type.CommonException;
 import org.dongguk.vsa.modeul.modeullak.domain.mysql.Modeullak;
@@ -12,8 +13,11 @@ import org.dongguk.vsa.modeul.user.domain.mysql.User;
 import org.dongguk.vsa.modeul.user.domain.mysql.UserModeullak;
 import org.dongguk.vsa.modeul.user.repository.mysql.UserModeullakRepository;
 import org.dongguk.vsa.modeul.user.repository.mysql.UserRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -23,6 +27,8 @@ public class CreateModeullakUserService implements CreateModeullakUserUseCase {
     private final UserRepository userRepository;
     private final ModeullakRepository modeullakRepository;
     private final UserModeullakRepository userModeullakRepository;
+
+    private final RabbitTemplate rabbitTemplate;
 
     @Override
     public void execute(UUID accountId, CreateModeullakUserRequestDto requestDto) {
@@ -49,6 +55,23 @@ public class CreateModeullakUserService implements CreateModeullakUserUseCase {
                 .build();
 
         userModeullakRepository.save(userModeullak);
+
+
+        int randomAdjectiveIndex = new Random().nextInt(Constants.ADJECTIVES.size());
+        int randomAnimalIndex = new Random().nextInt(Constants.ANIMALS.size());
+
+        rabbitTemplate.convertAndSend(
+                Constants.MODEUL_EXCHANGE_NAME,
+                String.format("modeullaks.%d.users", modeullak.getId()),
+                Map.of(
+                        "type", "USER",
+                        "data", Map.of(
+                                "id", user.getId(),
+                                "nickname", Constants.ADJECTIVES.get(randomAdjectiveIndex) + " " + Constants.ANIMALS.get(randomAnimalIndex),
+                                "profile_image_url", "temp"
+                        )
+                )
+        );
     }
 
     private Boolean isDuplicatedResource(User user, Modeullak modeullak) {
